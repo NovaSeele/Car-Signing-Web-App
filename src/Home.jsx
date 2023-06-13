bf5c8053e3d596eeab2aacd31e90b02ee307aef7
NovaSeele <iovn262003@gmail.com>
2023-06-13 19:11:52 +0700

    Update Home text

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavBar } from "./components/navBar/NavBar";
+import { Footer } from "./components/navBar/Footer";
 import { BrowserRouter as Route, Link, Routes } from "react-router-dom";
 import "./Home.css";
 
@@ -17,7 +18,6 @@ export const Home = () => {
           </div>
         </div>
 
-
         <div className="main-content">
           <div className="relax-content">
             <label className="relax-header"></label>
@@ -29,99 +29,58 @@ export const Home = () => {
             <div className="grid-content-left">
               <div className="left-block">
                 <div className="block-text">
-                  <label className="about">About</label>
-                  <h3 className="block-heading">Content 1</h3>
-                  <p className="block-main-text">
-                    Assist Registration Department
-                  </p>
+                  <label className="about">Thống kê</label>
+                  <h3 className="block-heading">
+                    Bảng thống kê thông tin đăng kiểm
+                  </h3>
+                  <p className="block-main-text">Xem danh sách các xe được đăng kiểm. Thông tin về xe và người dùng.</p>
                 </div>
                 <div className="block-button-div">
-                  <button className="block-button">Learn More</button>
+                  <button className="block-button">
+                    <Link to="/infotable">
+                      <span>Thống Kê</span>
+                    </Link>
+                  </button>
                 </div>
               </div>
               <div className="image-block-right">
-                <img src="src/assets/car.jpg" alt="Pic" width="100%" height="100%" />
+                <img
+                  src="src/assets/car.jpg"
+                  alt="Pic"
+                  width="100%"
+                  height="100%"
+                />
               </div>
             </div>
 
             <div className="grid-content-right">
               <div className="image-block-left">
-                <img src="src/assets/xe.jpg" alt="Pic" width="100%" height="100%" />
+                <img
+                  src="src/assets/xe.jpg"
+                  alt="Pic"
+                  width="100%"
+                  height="100%"
+                />
               </div>
               <div className="right-block">
                 <div className="block-text">
-                  <label className="about">About</label>
-                  <h3 className="block-heading">Content 2</h3>
-                  <p className="block-main-text">
-                    Assist Registration Center
-                  </p>
+                  <label className="about">Khai Báo</label>
+                  <h3 className="block-heading">Khai báo thông tin đăng kiêm xe</h3>
+                  <p className="block-main-text">Hệ thống đơn ghi nhận đăng kiểm xe online</p>
                 </div>
                 <div className="block-button-div">
-                  <button className="block-button">Learn More</button>
+                  <button className="block-button">
+                    <Link to="/formmain">
+                      <span>Khai Báo</span>
+                    </Link>
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <footer className="footer-distributed">
-        <div className="footer-left">
-          <h3>
-            Register<span>Total</span>
-          </h3>
-          <p className="footer-links">
-            <a href="#" className="link-1">
-              Home
-            </a>
-            <a href="#">Blog</a>
-            <a href="#">Pricing</a>
-            <a href="#">About</a>
-            <a href="#">Faq</a>
-            <a href="#">Contact</a>
-          </p>
-          <p className="footer-company-name">Company Name © 2015</p>
-        </div>
-
-        <div className="footer-center">
-          <div>
-            <i className="ri-map-pin-line"></i>
-            <p>
-              <span>36 Dich Vong Hau</span> Hanoi
-            </p>
-          </div>
-          <div>
-            <i className="ri-phone-line"></i>
-            <p>0999.999.999</p>
-          </div>
-          <div>
-            <i className="ri-mail-line"></i>
-            <p>
-              <a href="mailto:support@company.com">registrytotal@gmail.com</a>
-            </p>
-          </div>
-        </div>
-
-        <div className="footer-right">
-          <p className="footer-company-about">
-            <span>About the company</span>
-            RegistryTotal is a web application developed to support the Registration Deparment and registration centers nationwide in managing vehicle inspections for automobiles.
-          </p>
-          <div className="footer-icons">
-            <a href="#">
-              <i className="ri-facebook-fill"></i>
-            </a>
-            <a href="#">
-              <i className="ri-twitter-fill"></i>
-            </a>
-            <a href="#">
-              <i className="ri-linkedin-fill"></i>
-            </a>
-            <a href="#">
-              <i className="ri-github-fill"></i>
-            </a>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
